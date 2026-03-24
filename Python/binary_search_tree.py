fb8cc9c1ad80c07a6ccd631321340058bcb79ce4
Patrick <pwheeler636@gmail.com>
2026-03-23 22:56:48 -0500

    add a binary search tree

---
+++ Python/binary_search_tree.py
@@ -0,0 +1,57 @@
+# https://www.w3schools.com/python/python_dsa_binarysearchtrees.asp
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def inOrderTraversal(node):
+    if node == None:
+        return
+    inOrderTraversal(node.left)
+    print(node.data, end=", ")
+    inOrderTraversal(node.right)
+
+def search(node, target):
+    if node == None:
+        return None
+    elif node.data == target:
+        return node
+    elif target < node.data:
+        return search(node.left, target)
+    else:
+        return search(node.right, target)
+
+def insert(node, data):
+    if node == None:
+        return Node(data)
+    else: 
+        if data < node.data:
+            node.left = insert(node.left, data)
+        elif data > node.data:
+            node.right = instert(node.right, data)
+    return node
+
+def delete(node, data):
+    if not node:
+        return None
+    
+    if data < node.data:
+        node.left = delete(node.left, data)
+    elif data > node.data:
+        node.right = delete(node.right, data)
+    else:
+        if not node.left:
+            temp = node.right
+            node = None
+            return temp
+        elif not node.right:
+            temp = node.left
+            node = None
+            return temp
+
+        node.data = minValueNode(node.right).data
+        node.right = delete(node.right, node.data)
+    return node
+    
