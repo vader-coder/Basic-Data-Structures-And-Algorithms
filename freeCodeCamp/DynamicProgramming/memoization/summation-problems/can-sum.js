ca70d8a290362a18cb343a6c5a5cb58168bb2e08
Patrick <pwheeler636@gmail.com>
2026-05-01 15:13:27 -0500

    fixed a typo in can-sum.js

--- freeCodeCamp/DynamicProgramming/memoization/summation-problems/can-sum.js
+++ freeCodeCamp/DynamicProgramming/memoization/summation-problems/can-sum.js
@@ -5,7 +5,7 @@
     The function should return a boolean indicating whether or not it is possible 
     to generate the target using numbers from the array. 
     
-    You may use an element of the array as many times as needed. You may assumet
+    You may use an element of the array as many times as needed. You may assume
     that all input numbers are nonnegative.
 */
 
