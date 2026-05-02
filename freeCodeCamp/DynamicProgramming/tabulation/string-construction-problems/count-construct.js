295ce510233ea7d475a8a93ec9f0f83a9b7bd66f
Patrick <pwheeler636@gmail.com>
2026-05-01 21:58:45 -0500

    add a tabulation solution for the count-construct problem

---
+++ freeCodeCamp/DynamicProgramming/tabulation/string-construction-problems/count-construct.js
@@ -0,0 +1,42 @@
+/*
+    Write a function `countConstruct(target, words)` that takes in a target string and 
+    an array of strings as arguments. 
+
+    The function should return the number of ways that the target string can be 
+    constructed by concatenating elements of the words array.
+
+    You may reuse elements of the words array as many times as needed.   
+*/
+
+function countConstruct(target, words) {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+    
+    for (let i = 0; i <= target.length; i++) {
+        for (let word of words) {
+            if (target.slice(i, i + word.length) === word) {
+                table[i + word.length] += table[i];
+            }
+        }
+    }
+
+    return table[target.length];
+}
+
+/*
+    m = target.length, n = words.length
+    O(n*m^2) time complexity, O(m) space complexity
+*/
+
+console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"])); // 2
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // 1
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // 0
+console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); // 4
+console.log(countConstruct("e".repeat(50) + "f", [
+    "e",
+    "ee",
+    "eee", 
+    "eeee",
+    "eeeee",
+    "eeeeee"
+])); // 0
